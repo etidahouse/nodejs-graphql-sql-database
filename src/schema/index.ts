02c09bad6ba8c0ad0ed665c2028a41328be22074
etidahouse <e-begon@hotmail.fr>
2022-04-24 10:29:15 +0200

    Add players, find and count queries

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,12 +1,18 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import hello from './queries/hello';
 import player from './queries/player';
+import players from './queries/players';
+import find from './queries/find'
+import count from './queries/count';
 
 const query = new GraphQLObjectType({
     name: 'Query',
     fields: (): any => ({
       hello,
-      player
+      player,
+      players,
+      find,
+      count
     }),
   });
 
