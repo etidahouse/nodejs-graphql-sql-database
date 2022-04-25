125dee51f55a1d50189a6d1d47b64d60672e33ff
etidahouse <e-begon@hotmail.fr>
2022-04-25 09:17:23 +0200

    Split queries

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,20 +1,5 @@
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-import hello from './queries/hello';
-import player from './queries/player';
-import players from './queries/players';
-import find from './queries/find'
-import count from './queries/count';
-
-const query = new GraphQLObjectType({
-    name: 'Query',
-    fields: (): any => ({
-      hello,
-      player,
-      players,
-      find,
-      count
-    }),
-  });
+import { GraphQLSchema } from 'graphql';
+import query from './query';
 
 export default new GraphQLSchema({
   query,
---
+++ src/schema/query.ts
@@ -0,0 +1,19 @@
+import { GraphQLObjectType } from "graphql";
+import hello from './queries/hello';
+import player from './queries/player';
+import players from './queries/players';
+import find from './queries/find'
+import count from './queries/count';
+
+const query = new GraphQLObjectType({
+    name: 'Query',
+    fields: (): any => ({
+      hello,
+      player,
+      players,
+      find,
+      count
+    }),
+  });
+
+  export default query;
